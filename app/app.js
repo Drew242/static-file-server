077364db2a41de9aed2bce030fb9b61c441d7c25
Drew Conly <atconly@gmail.com>
2016-08-02 20:50:42 -0600

    dynamic file load with params

--- app/app.js
+++ app/app.js
@@ -12,21 +12,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-var fileContents = fs.readFile('./app/data.txt', function(err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    app.get('/', function(req, res){
-      res.header('Content-Type', 'text/html');
-      res.send(`<p>${data}<p>`);
-    });
-  }
+app.get('/:filename', function(req, res){
+  var dataFile     = req.params.filename;
+  var fileContents = fs.readFile(`./app/${dataFile}`, function(err, data) {
+    console.log(dataFile);
+    res.header('Content-Type', 'text/html');
+    res.send(`<p>${data}<p>`);
+  })
 });
 
 // Routes \\
-// app.get('/', function(req, res){
-//
-// });
+app.get('/', function(req, res){
+  res.send('hello squirrel');
+});
 
 // Creating Server and Listening for Connections \\
 var port = 3000
