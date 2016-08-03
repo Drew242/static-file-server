1d48cb2ac00998b0353ed4236975ed4c97334de9
Drew Conly <atconly@gmail.com>
2016-08-02 20:29:29 -0600

    make data read async

--- app/app.js
+++ app/app.js
@@ -12,13 +12,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
-var fileContents = fs.readFileSync('./app/data.txt');
+var fileContents = fs.readFile('./app/data.txt', function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    app.get('/', function(req, res){
+      res.header('Content-Type', 'text/html');
+      res.send(`<p>${data}<p>`);
+    });
+  }
+});
 
 // Routes \\
-app.get('/', function(req, res){
-  res.header('Content-Type', 'text/html');
-  res.send(`<p>${fileContents}<p>`);
-});
+// app.get('/', function(req, res){
+//
+// });
 
 // Creating Server and Listening for Connections \\
 var port = 3000
